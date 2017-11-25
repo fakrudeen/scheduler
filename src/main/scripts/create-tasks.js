eed0ab9008a45b643e0d8bd7d83d9bfde50c5722
fakrudeen <fakrudeen_ali_ahmed@yahoo.com>
2017-11-24 22:12:56 -0800

    Reduce max sleep time to 100 seconds.

--- src/main/scripts/create-tasks.js
+++ src/main/scripts/create-tasks.js
@@ -5,5 +5,5 @@
  */
 var db = connect('127.0.0.1:27017/scheduler');
 for (i = 0; i < 100; i++) {
-    db.tasks.insert({'taskname':'task'+i, 'sleeptime': Math.floor(Math.random()*10000),'status':'created'});
+    db.tasks.insert({'taskname':'task'+i, 'sleeptime': Math.floor(Math.random()*100),'status':'created'});
 }
